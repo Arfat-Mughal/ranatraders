a622b5f18efaa01a7f55d9adc67ae6f0ade712f6
Arfat <57156681+Arfat-Mughal@users.noreply.github.com>
2024-05-31 15:50:06 +0500

    Update welcome.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,12 +71,12 @@
         <div class="container position-relative" data-aos="fade-up" data-aos-delay="100">
             <div class="row justify-content-center">
                 <div class="col-xl-7 col-lg-9 text-center">
-                    <h1>One Page Bootstrap Website Template</h1>
-                    <h2>We are a team of talented designers</h2>
+                    <h1>Rana Traders Pvt Limited</h1>
+                    <h2>We deal all kind of Petroleum Services</h2>
                 </div>
             </div>
             <div class="text-center">
-                <a href="#about" class="btn-get-started scrollto">Get Started</a>
+                <a href="#contact" class="btn-get-started scrollto">Contact Us</a>
             </div>
 
             <div class="row icon-boxes">
@@ -672,12 +672,13 @@
         </section><!-- End Contact Section -->
     </main>
 
-    <!-- resources/views/layouts/footer.blade.php -->
     <!-- ======= Footer ======= -->
     <footer id="footer">
+
         <div class="footer-top">
             <div class="container">
                 <div class="row">
+
                     <div class="col-lg-3 col-md-6 footer-contact">
                         <h3>OnePage</h3>
                         <p>
@@ -718,29 +719,16 @@
                             <input type="email" name="email"><input type="submit" value="Subscribe">
                         </form>
                     </div>
+
                 </div>
             </div>
         </div>
 
         <div class="container d-md-flex py-4">
-            <div class="me-md-auto text-center text-md-start">
+            <div class="md-auto text-center text-md-start">
                 <div class="copyright">
-                    &copy; Copyright <strong><span>OnePage</span></strong>. All Rights Reserved
+                    &copy; Copyright <strong><span>Rana Traders</span></strong>. All Rights Reserved
                 </div>
-                <div class="credits">
-                    <!-- All the links in the footer should remain intact. -->
-                    <!-- You can delete the links only if you purchased the pro version. -->
-                    <!-- Licensing information: https://bootstrapmade.com/license/ -->
-                    <!-- Purchase the pro version with working PHP/AJAX contact form: https://bootstrapmade.com/onepage-multipurpose-bootstrap-template/ -->
-                    Designed by <a href="https://bootstrapmade.com/">BootstrapMade</a>
-                </div>
-            </div>
-            <div class="social-links text-center text-md-right pt-3 pt-md-0">
-                <a href="#" class="twitter"><i class="bx bxl-twitter"></i></a>
-                <a href="#" class="facebook"><i class="bx bxl-facebook"></i></a>
-                <a href="#" class="instagram"><i class="bx bxl-instagram"></i></a>
-                <a href="#" class="google-plus"><i class="bx bxl-skype"></i></a>
-                <a href="#" class="linkedin"><i class="bx bxl-linkedin"></i></a>
             </div>
         </div>
     </footer><!-- End Footer -->
@@ -748,6 +736,7 @@
     <div id="preloader"></div>
     <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
             class="bi bi-arrow-up-short"></i></a>
+
 
     <!-- Vendor JS Files -->
     <script src="{{ asset('assets/vendor/purecounter/purecounter_vanilla.js') }}"></script>
